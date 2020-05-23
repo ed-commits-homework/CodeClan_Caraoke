83861ec11b387dac0f0bf9057a9864eb9356338b
Ed <ed-commits@github.com>
2020-05-23 10:21:14 +0100

    add a constructor for guest and test it

--- spec/guest_spec.rb
+++ spec/guest_spec.rb
@@ -2,5 +2,11 @@ require("minitest/autorun")
 require("minitest/reporters")
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+require_relative("../guest")
+
 class TestGuest < MiniTest::Test
+    def test_guest_name
+        ed = Guest.new("Ed")
+        assert_equal("Ed", ed.name)
+    end
 end
