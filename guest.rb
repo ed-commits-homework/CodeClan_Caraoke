ba79dd917af2de5916e9ee2c451c91ea1b28612a
Ed <ed-commits@github.com>
2020-05-23 10:46:44 +0100

    add a money attribute for guests and an ability to pay fees, if they have enough

--- guest.rb
+++ guest.rb
@@ -1,7 +1,17 @@
 class Guest
-    attr_reader :name
+    attr_reader :name, :money
 
-    def initialize(name)
+    def initialize(name, money = 0)
         @name = name
+        @money = money
+    end
+
+    def pay(fee)
+        if @money >= fee
+            @money -= fee
+            return fee
+        else
+            return 0
+        end
     end
 end
--- spec/guest_spec.rb
+++ spec/guest_spec.rb
@@ -9,4 +9,18 @@ class TestGuest < MiniTest::Test
         ed = Guest.new("Ed")
         assert_equal("Ed", ed.name)
     end
+
+    def test_guest_pay_fee
+        ed = Guest.new("Ed", 50)
+        assert_equal(ed.money, 50)
+        assert_equal(10, ed.pay(10))
+        assert_equal(ed.money, 40)
+    end
+    
+    def test_guest_cant_pay_fee
+        ed = Guest.new("Ed", 10)
+        assert_equal(ed.money, 10)
+        assert_equal(0, ed.pay(50))
+        assert_equal(ed.money, 10)
+    end
 end
