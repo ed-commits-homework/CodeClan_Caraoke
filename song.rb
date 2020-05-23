cb0a0e2a2c5f57976dfad43c342dc2d21be3d3af
Ed <ed-commits@github.com>
2020-05-23 10:36:59 +0100

    implement and test the song constructor

--- song.rb
+++ song.rb
@@ -1,2 +1,9 @@
 class Song
+    attr_reader :name, :author, :length
+
+    def initialize(name, author, length)
+        @name = name
+        @author = author
+        @length = length
+    end
 end
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -2,5 +2,13 @@ require("minitest/autorun")
 require("minitest/reporters")
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+require_relative("../song")
+
 class TestSong < MiniTest::Test
+    def test_song_initialize
+        song = Song.new("Sweet Caroline", "Neil Diamond", 3.21)
+        assert_equal("Sweet Caroline", song.name)
+        assert_equal("Neil Diamond", song.author)
+        assert_equal(3.21, song.length)
+    end
 end
